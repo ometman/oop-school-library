59c63d6538c60e8c0c34a4b86e1c2b21b94041ef
ometman <revomet@gmail.com>
2023-11-23 01:05:44 +0000

    Fix rubocop errors

--- student.rb
+++ student.rb
@@ -6,13 +6,15 @@ class Student < Person
   def initialize(age, classroom, name = 'UNKNOWN', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
-    classroom.add_student(self) if classroom
+    classroom&.add_student(self)
   end
+
   def classroom=(classroom)
     @classroom&.students&.delete(self)
     @classroom = classroom
     classroom.students << self if classroom
   end
+
   def play_hooky
     '¯\\_(ツ)_/¯'
   end
