746ef5ea11cfe0c9990a2044ff25f2d9e660a9a5
Kenneth Adutwum <kwamayadu@gmail.com>
2023-12-01 11:13:54 +0000

    Add test to project

--- person.rb
+++ person.rb
@@ -1,12 +1,8 @@
-require './nameable'
-require './rental'
-require './book'
-
 class Person < Nameable
   attr_accessor :name, :age, :id
   attr_reader :rentals, :parent_permission
 
-  def initialize(age, name, parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @age = age
@@ -15,21 +11,23 @@ class Person < Nameable
     @rentals = []
   end
 
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
   def can_use_services?
     of_age? || @parent_permission
   end
 
   def correct_name
-    @name
+    @name = @name.upcase
   end
 
-  def add_rental(date, book)
+  def add_rental(book, date)
     Rental.new(date, book, self)
-  end
-
-  private
-
-  def of_age?
-    @age >= 18
   end
 end
